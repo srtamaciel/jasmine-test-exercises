89f40c5ea26a49e6804f21d2ffd8ec78a0a2039d
Raquel Rodriguez <srtademaciel@gmail.com>
2021-06-09 19:39:16 +0200

    testin exercise

---
+++ src/cents-to-decimals.js
@@ -0,0 +1,12 @@
+const centsToDecimals = (centValue) => {
+  if (typeof centValue !== 'number' || isNaN(centValue)) {
+    return undefined;
+  }
+
+  let result = centValue / 100;
+
+  return result.toFixed(2) + '$';
+}
+
+// The following is required to make automated tests run. Please ignore it.
+if (typeof module !== 'undefined') module.exports = { centsToDecimals };
